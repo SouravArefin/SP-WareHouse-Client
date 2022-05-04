00fd1c83c9d5e37984c94b4ed44d075a651320ae
Sourav123 <khansourav58@gmail.com>
2022-05-04 22:13:17 +0600

    firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,13 +5,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  //   apiKey: "AIzaSyCu7XY2fiHg3T0a-KrDibSIbxOEmpfigro",
-  //   authDomain: "sp-warehouse.firebaseapp.com",
-  //   projectId: "sp-warehouse",
-  //   storageBucket: "sp-warehouse.appspot.com",
-  //   messagingSenderId: "855551625035",
-  //   appId: "1:855551625035:web:b531cfb8a8addd23114ace"
-  // };
+ 
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
